764de04f14e0e048055d071f904d7cc002a910ed
Larry Otieno <larryotieno51@gmail.com>
2025-03-06 00:16:17 +0300

    Adds functions continued

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+// Function declaration for saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function expression for mondayWork
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Function for wrapAdjective
+  function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  
